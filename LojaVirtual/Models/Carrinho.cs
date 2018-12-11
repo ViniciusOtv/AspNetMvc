4a467d9b821a815c0f318d0343b9d9bbf767eabe
aspnetmvc <aspnetmvc@LAB104501>
2018-12-11 21:57:54 -0200

    Alterações aula criando e  implementando sessão no carrinho.

---
+++ LojaVirtual/Models/Carrinho.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LojaVirtual.Models
+{
+    public class Carrinho
+    {
+        private Dictionary<int, CarrinhoItem> Itens;
+
+        public Carrinho()
+        {
+            Itens = new Dictionary<int, CarrinhoItem>();
+        }
+
+        public void Add(Produto produto)
+        {
+            if (Itens.ContainsKey(produto.Id))
+            {
+                Itens[produto.Id].Quantidade++;
+            }
+            else
+            {
+                var carrinhoItem = new CarrinhoItem(produto.Id,
+                    produto.Nome,
+                    produto.Preco);
+
+                Itens.Add(produto.Id, carrinhoItem);
+            }
+        }
+    }
+}
